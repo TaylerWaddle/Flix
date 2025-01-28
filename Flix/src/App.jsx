46343e803787a74bea9afedb307a0486c6557f22
Tayler Waddle <taylerwaddle@gmail.com>
2025-01-27 22:06:40 -0700

    added hero section

--- Flix/src/App.jsx
+++ Flix/src/App.jsx
@@ -1,8 +1,26 @@
+/* eslint-disable react/no-unescaped-entities */
+import { useState } from "react";
+import Search from "./components/search";
+
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
+      </div>
+    </main>
   );
 };
 
